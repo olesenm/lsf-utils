bc621b1b3fe63944a2d117cb10f30a1737656152
Mark Olesen <Mark.Olesen@faurecia.com>
2011-12-12 10:53:48 +0100

    moved from Affero license to normal GPL

--- src/markutil/HttpServer.cpp
+++ src/markutil/HttpServer.cpp
@@ -5,16 +5,16 @@ License
     This file is part of lsf-utils.
 
     lsf-utils is free software: you can redistribute it and/or modify it under
-    the terms of the GNU Affero General Public License as published by the
+    the terms of the GNU General Public License as published by the
     Free Software Foundation, either version 3 of the License,
     or (at your option) any later version.
 
     lsf-utils is distributed in the hope that it will be useful, but
     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
     or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU Affero General Public License for more details.
+    See the GNU General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
+    You should have received a copy of the GNU General Public License
     along with lsf-utils. If not, see <http://www.gnu.org/licenses/>.
 
 \*---------------------------------------------------------------------------*/
